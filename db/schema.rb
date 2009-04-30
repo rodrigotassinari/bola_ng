d766925ff6a8f78cc6a64d7571e20574b91e084c
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2009-04-30 00:03:07 -0300

    Adiciona plugin acts_as_taggable_on_steroids.

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090430030146) do
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
 end
