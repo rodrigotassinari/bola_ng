a832260e076d9a40465fa8f30dd9b840b06be473
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2009-05-04 23:40:24 -0300

    Cria slugs para posts de artigos.

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,12 +19,24 @@ class Post < ActiveRecord::Base
   validates_presence_of :body, :if => :is_article?
   validates_uniqueness_of :identifier, :scope => :service_id
 
-  #before_create :slugify_if_article # TODO
+  before_create :slugify_if_article
   #before_create :summarize_if_article # TODO
 
   # returns true if this post is associated with a BlogService
   def is_article?
     self.service.class == BlogService
   end
+
+  def to_param
+    is_article? ? "#{self.id}-#{self.slug}" : "#{self.id}"
+  end
+
+  protected
+
+    def slugify_if_article
+      if is_article?
+        self.slug = PermalinkFu.escape(self.title)[0..60]
+      end
+    end
 
 end
