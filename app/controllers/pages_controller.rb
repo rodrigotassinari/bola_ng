3ac34f7e96972afefe415d48d2a18d7443d91e61
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2009-05-06 17:20:29 -0300

    inicia controller de páginas estáticas.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,24 @@
 class PagesController < ApplicationController
 
+  # GET /about
+  # Via: about_path
+  # Avaiable: all
+  #
+  # Shows the 'about' page.
   def about
+    @current_tab = 'about'
+    @page_title = "Sobre"
     # TODO
   end
 
+  # GET /contact
+  # Via: contact_path
+  # Avaiable: all
+  #
+  # Shows the 'contact' page.
   def contact
+    @current_tab = 'contact'
+    @page_title = "Contato"
     # TODO
   end
   
