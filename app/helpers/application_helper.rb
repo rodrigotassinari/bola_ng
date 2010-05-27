15a72ad34e4146a77521965f2a61e9b870f65b7f
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2010-05-27 15:20:40 -0300

    Monitoramento de acesso via getclicky.com

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -78,6 +78,25 @@ module ApplicationHelper
       } catch(err) {}</script>
     eos
   end
+  
+  def getclicky_javascript(site_id)
+    return unless Rails.env.production?
+    <<-eos
+      <script type="text/javascript">
+      var clicky = { log: function(){ return; }, goal: function(){ return; }};
+      var clicky_site_id = #{site_id};
+      (function() {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = ( document.location.protocol == 'https:' ? 'https://static.getclicky.com' : 'http://static.getclicky.com' ) + '/js';
+        ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+      })();
+      </script>
+      <a title="Web Analytics" href="http://getclicky.com/218057"></a>
+      <noscript><p><img alt="Clicky" width="1" height="1" src="http://in.getclicky.com/218057ns.gif" /></p></noscript>
+    eos
+  end
 
   def random_phrase
     [
