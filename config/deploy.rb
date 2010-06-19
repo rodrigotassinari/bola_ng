01bcde131de655138e63c2b80406dc20fce0b535
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2010-06-19 13:29:54 -0300

    Configura deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,71 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+## configuração
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "bola_ng"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :repository,  "git@github.com:rtopitt/bola_ng.git"
+set :branch, "production"
+set :deploy_via, :remote_cache
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :domain, "pittlandia.net"
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+role :web, "97.107.135.29"                          # Your HTTP server, Apache/etc
+role :app, "97.107.135.29"                          # This may be the same as your `Web` server
+role :db,  "97.107.135.29", :primary => true        # This is where Rails migrations will run
+
+set :deploy_to, "/home/rodrigo/www/pittlandia.net"
+set :rails_env, "production"
+
+set :use_sudo, false
+set :user, "rodrigo"
+
+set :ruby_path, "/opt/ree/bin/ruby"
+set :rake_path, "/opt/ree/bin/rake"
+set :rake,      "/opt/ree/bin/rake"
+set :gem_path,  "/opt/ree/bin/gem"
+set :god_path,  "/opt/ree/bin/god"
+
+set :port, 30000 # SSH port
+default_run_options[:pty] = true
+
+## Tasks
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Prepare shared files"
+  task :shared_setup do
+    run "mkdir -p #{shared_path}/config"
+    run "cp #{latest_release}/config/database.yml.example #{shared_path}/config/database.yml"
+    run "cp #{latest_release}/config/application.yml.example #{shared_path}/config/application.yml"
+  end
+
+  desc "Symlink shared configs and folders on each release."
+  task :shared_symlink do
+    run "ln -nfs #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/application.yml #{latest_release}/config/application.yml"
+  end
+end
+
+namespace :bundler do
+  desc "Run bundle install on app root"
+  task :install do
+    run "cd #{current_path} && bundle install --without test development"
+    #run "cd #{release_path} && bundle install --without test development"
+  end
+end
+
+# Callbacks
+
+after "deploy:update_code", "deploy:shared_symlink"
+after "deploy:update_code", "bundler:install"
+
+after "deploy", "deploy:cleanup"
+after "deploy:migrations", "deploy:cleanup"
+
